a4e5a4f0356e8785483a1e34cde2940caabef85e
Rishabh Agrawal <rishabh120296@gmail.com>
2018-07-13 15:34:31 +0530

    Changed logic for csv to array

--- aggregate.js
+++ aggregate.js
@@ -12,76 +12,27 @@ const aggregate = (filepath) => {
   const inputFile = fs.readFileSync(filepath, 'utf8');
 
   //  console.log(inputFile);
+  function processData(allText) {
+    const allTextLines = allText.split(/\r\n|\n/);
+    const headers = allTextLines[0].split(',');
+    const lines = [];
 
-  function CSVToArray(strData, strDelimiter) {
-    // Create a regular expression to parse the CSV values.
-    const objPattern = new RegExp(
-      (
-        // Delimiters.
-        `(\\${strDelimiter}|\\r?\\n|\\r|^)`
-
-        // Quoted fields.
-        + '(?:"([^"]*(?:""[^"]*)*)"|'
-
-        // Standard fields.
-        + `([^"\\${strDelimiter}\\r\\n]*))`
-      ),
-      'gi',
-    );
-
-
-    // Create an array to hold our data. Give the array
-    // a default empty first row.
-    const arrData = [[]];
-
-    // Create an array to hold our individual pattern
-    // matching groups.
-    let arrMatches = null;
-
-
-    // Keep looping over the regular expression matches
-    // until we can no longer find a match.
-    arrMatches = objPattern.exec(strData);
-    while (arrMatches) {
-      // Get the delimiter that was found.
-      const strMatchedDelimiter = arrMatches[1];
-
-      // Check to see if the given delimiter has a length
-      // (is not the start of string) and if it matches
-      // field delimiter. If id does not, then we know
-      // that this delimiter is a row delimiter.
-      if (strMatchedDelimiter.length && strMatchedDelimiter !== strDelimiter) {
-        // Since we have reached a new row of data,
-        // add an empty row to our data array.
-        arrData.push([]);
+    for (let i = 1; i < allTextLines.length; i += 1) {
+      const regex = /["]/g;
+      const data = allTextLines[i].replace(regex, '').split(',');
+      if (data.length === headers.length) {
+        const tarr = [];
+        for (let j = 0; j < headers.length; j += 1) {
+          tarr.push(data[j]);
+        }
+        lines.push(tarr);
       }
-      let strMatchedValue;
-      // Now that we have our delimiter out of the way,
-      // let's check to see which kind of value we
-      // captured (quoted or unquoted).
-      if (arrMatches[2]) {
-        // We found a quoted value. When we capture
-        // this value, unescape any double quotes.
-        strMatchedValue = arrMatches[2].replace(
-          new RegExp('""', 'g'),
-          '"',
-        );
-      } else {
-        // We found a non-quoted value.
-        [strMatchedValue] = [arrMatches[3]];
-      }
-      // Now that we have our value string, let's add
-      // it to the data array.
-      arrData[arrData.length - 1].push(strMatchedValue);
-      arrMatches = objPattern.exec(strData);
     }
-
-    // Return the parsed data.
-    return (arrData);
+    return lines;
   }
+  const outputFile = processData(inputFile);
 
-  const outputFile = CSVToArray(inputFile, ',');
-  //  console.log(outputFile[1][0]);
+  //  console.log(outputFile);
 
   const finaloutput = [['South America', 0, 0],
     ['Oceania', 0, 0],
